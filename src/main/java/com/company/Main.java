8eb0c13e3eb53acc010248952e124ed388c6a46a
khomyakeugene <khomyak_eugene@yahoo.com>
2016-05-02 15:10:58 +0300

    JEE Unit 6.1.10 CaseStudy

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -8,27 +8,33 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Main {
-    private static final String DRIVER_CLASS_NAME = "org.postgresql.Driver";
+    private static final String DATABASE_DRIVER_CLASS_NAME = "org.postgresql.Driver";
+    private static final String DATABASE_URL = "jdbc:postgresql://localhost:5432/company";
+    private static final String DATABASE_USER_NAME = "user";
+    private static final String DATABASE_USER_PASSWORD = "1111";
 
     private static final Logger LOGGER =  LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
         loadDriver();
-        try(Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/company")) {
+        LOGGER.info("Connecting to DB");
+        try(Connection connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USER_NAME,
+                DATABASE_USER_PASSWORD)) {
+            LOGGER.info("Successfully connected to DB ");
 
         } catch (SQLException e) {
-            e.printStackTrace();
+            LOGGER.error("Exception occurred while connecting to DB: " + DATABASE_URL, e);
         }
 
     }
 
     private static void loadDriver() {
         try {
-            LOGGER.info("Loading JDBC driver: " + DRIVER_CLASS_NAME);
-            Class.forName(DRIVER_CLASS_NAME);
+            LOGGER.info("Loading JDBC driver: " + DATABASE_DRIVER_CLASS_NAME);
+            Class.forName(DATABASE_DRIVER_CLASS_NAME);
             LOGGER.info("Driver loaded successfully");
         } catch (ClassNotFoundException e) {
-            LOGGER.error("Cannot find driver: " + DRIVER_CLASS_NAME, e);
+            LOGGER.error("Cannot find driver: " + DATABASE_DRIVER_CLASS_NAME, e);
             throw new RuntimeException(e);
         }
     }
