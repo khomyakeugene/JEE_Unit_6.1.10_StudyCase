33a3f411bb2cba89976ccb41fcd8efff09a27019
khomyakeugene <khomyak_eugene@yahoo.com>
2016-05-02 14:03:05 +0300

    JEE Unit 6.1.10 CaseStudy

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 
 public class Main {
     private static final String DRIVER_CLASS_NAME = "org.postgresql.Driver";
+
     private static final Logger LOGGER =  LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
@@ -14,7 +15,9 @@ public class Main {
 
     private static void loadDriver() {
         try {
+            LOGGER.info("Loading JDBC driver: " + DRIVER_CLASS_NAME);
             Class.forName(DRIVER_CLASS_NAME);
+            LOGGER.info("Driver loaded successfully");
         } catch (ClassNotFoundException e) {
             LOGGER.error("Cannot find driver: " + DRIVER_CLASS_NAME, e);
             throw new RuntimeException(e);
