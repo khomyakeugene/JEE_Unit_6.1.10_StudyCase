e081490dc3043af888b092c0bd5c22bffeff4e01
khomyakeugene <khomyak_eugene@yahoo.com>
2016-05-02 12:24:18 +0300

    Initial commit

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        loadDriver();
+
+    }
+
+    private static void loadDriver() {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
