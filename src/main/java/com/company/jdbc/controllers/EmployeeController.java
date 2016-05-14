3e41571442465d49df8ef754520e7d772a704fad
khomyakeugene <khomyak_eugene@yahoo.com>
2016-05-14 23:28:57 +0300

    JEE Unit 6.1.10 CaseStudy

---
+++ src/main/java/com/company/jdbc/controllers/EmployeeController.java
@@ -0,0 +1,18 @@
+package com.company.jdbc.controllers;
+
+import com.company.jdbc.model.Employee;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import java.util.List;
+
+/**
+ * Created by Yevhen on 14.05.2016.
+ */
+public class EmployeeController {
+
+    private PlatformTransactionManager txManager;
+
+    public List<Employee> getAllEmployees() {
+
+    }
+}
